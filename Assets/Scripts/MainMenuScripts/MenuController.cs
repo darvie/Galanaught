65502b8e002cf0bac5cfb590689f584989409a7f
ssmith86 <samuel.jp.smith@gmail.com>
2025-03-23 15:11:20 -0700

    Buttons and menu functionality

--- Assets/Scripts/MainMenuScripts/MenuController.cs
+++ Assets/Scripts/MainMenuScripts/MenuController.cs
@@ -1,9 +1,15 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System.Collections;
+using TMPro;
 
 public class MenuController : MonoBehaviour
 {
+    [SerializeField] private TMP_Text volumeTextValue = null;
+    [SerializeField] private Slider volumeSliderValue = null;
+    [SerializeField] private GameObject confirmationPrompt = null;
+    
     public string _newGameLevel;
     private string levelToLoad;
 
@@ -15,6 +21,25 @@ public class MenuController : MonoBehaviour
     public void PlayGame()
     {
         SceneManager.LoadScene(levelToLoad);
+    }
+
+    public void SetVolume(float volume)
+    {
+        AudioListener.volume = volume;
+        volumeTextValue.text = volume.ToString("0.0");
+    }
+
+    public void VolumeApply()
+    {
+        PlayerPrefs.SetFloat("masterVolume", AudioListener.volume);
+            StartCoroutine(confirmationBoX());
+    }
+
+    public IEnumerator confirmationBoX()
+    {
+        confirmationPrompt.SetActive(true);
+        yield return new WaitForSeconds(2);
+        confirmationPrompt.SetActive(false);
     }
 
     public void QuitGame()
