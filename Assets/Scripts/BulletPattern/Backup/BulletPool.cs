51ff9995c2242a4433820136a52304e6f6f8254e
Graves067 <c.a.gillespie9@gmail.com>
2025-03-23 15:26:48 -0700

    bullets fire

--- Assets/Scripts/BulletPattern/Backup/BulletPool.cs
+++ Assets/Scripts/BulletPattern/Backup/BulletPool.cs
@@ -13,7 +13,7 @@ public class BulletPool : MonoBehaviour
 
     private void Awake()
     {
-      BulletPool instance = this;
+        BulletPoolInstance = this;
     }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
