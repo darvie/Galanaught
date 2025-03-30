a648a3f5bf3294d64c8d5b6d32cc9a3b08b75258
ssmith86 <samuel.jp.smith@gmail.com>
2025-03-30 13:59:14 -0700

    bullets now move along local Y axis
    bullet pattern for one enemy is off now, but bullets fly forward now

--- Assets/Scripts/Bullet/BulletPattern/EBullet.cs
+++ Assets/Scripts/Bullet/BulletPattern/EBullet.cs
@@ -1,8 +1,8 @@
+
 using UnityEngine;
 
 public class EBullet : MonoBehaviour
 {
-    
     public float bulletLife = 1f;  // Defines how long before the bullet is destroyed
     public float rotation = 0f;
     public float speed = 1f;
@@ -22,14 +22,14 @@ public class EBullet : MonoBehaviour
         if (timer > bulletLife) Destroy(this.gameObject);
         timer += Time.deltaTime;
         transform.position = Movement(timer);
-        
+
     }
 
     private Vector2 Movement(float timer)
     {
-        // Moves right according to the bullet's rotation
-        float x = timer * speed * transform.right.x;
-        float y = timer * speed * transform.right.y;
+        // Moves up according to the bullet's local Y-axis
+        float x = timer * speed * transform.up.x;
+        float y = timer * speed * transform.up.y;
         return new Vector2(x + spawnPoint.x, y + spawnPoint.y);
     }
 
