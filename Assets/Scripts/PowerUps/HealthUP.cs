5a83895eb4eabdcd6d796a0d2824648b3bff9009
darvie <ddhill01@student.ubc.ca>
2025-03-31 17:14:34 -0700

    added sfx to powerups

--- Assets/Scripts/PowerUps/HealthUP.cs
+++ Assets/Scripts/PowerUps/HealthUP.cs
@@ -5,6 +5,9 @@ public class HealthUP : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other)
     {
         //Debug.Log($"collided with{other.gameObject.name}");
+
+        AudioManager.Instance.PlayPowerUpInvulnerabilitySFX();
+
         if (other.gameObject.CompareTag("Player"))
         {
             other.gameObject.GetComponent<PlayerController>().HEALPOWERUP();
